fbbfa05f83f0877774349302d3982d2e87386f9b
ZhouSusan <szhou089@gmail.com>
2022-12-18 16:29:16 -0800

    Added the Spread Operator

--- app.js
+++ app.js
@@ -6,4 +6,13 @@ var x = function (x, y) {
 }
 
 //ES6
-const y = (x, y) => x * 6; 
+const y = (x, y) => x * y; 
+
+
+//Spread Operator
+const cars1 = ["Saab", "Volvo", "BMW"];
+const cars2 = ["Saab", "Volvo", ...["BMW"]];
+
+const numbers = [23, 55, 21, 87, 56];
+let minValue = Math.min(...numbers);
+let maxValue = Math.max(...numbers);
